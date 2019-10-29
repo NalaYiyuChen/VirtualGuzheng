2019236362d45823fc78c59aa29b1a29d65873bc
NalaYiyuChen <52183074+NalaYiyuChen@users.noreply.github.com>
2019-10-28 18:11:50 -0700

    Complete

---
+++ script.js
@@ -0,0 +1,9 @@
+// /* Code to have strings react to mouse over. */
+// window.onload = function(){
+//   document.getElementById("6").onmouseover = function(evt){
+//     document.getElementById("6").height = 2000;
+//   }
+//   document.getElementById("6").onmouseout = function(evt){
+//     document.getElementById("6").height = 5;
+//   }
+// }
